5ac23a6e1173f861dfbb1a3c4afad897bd0f9188
Leon Niesler <leon.niesler@gmail.com>
2020-09-24 15:26:19 +0200

    added spring to form

--- src/components/AddCardForm.js
+++ src/components/AddCardForm.js
@@ -1,25 +1,34 @@
 import React from 'react'
 import { X } from 'react-feather';
 import { useForm } from "react-hook-form";
+import { useSpring, animated as a} from 'react-spring'
 
 export default function AddCardForm(props) {
   const { register, handleSubmit, reset} = useForm();
+  const expandStyle = useSpring({
+    from: { left: '0%', top: '0%', width: '100%', height: '100%'},
+    to: { left: '0%', top: '0%', width: '0%', height: '0%'}});
 
   const onSubmit = (data) => {
     props.addCard([data.original.trim(), data.translation.trim()]);
     reset()
   };
+
+  const closeForm = () => {
+
+    props.closeAddCardForm()
+  }
     
   return (
-    <div className="newCardContainer" style={props.style}>
+    <a.div className="newCardContainer" style={props.style, expandStyle}>
       <div className="newCardForm">
-        <button id="hideFormButton" onClick={() => props.closeAddCardForm()} ><X className="buttonIcon" color="darkgrey" /></button>
+        <button id="hideFormButton" onClick={() => closeForm} ><X className="buttonIcon" color="darkgrey" /></button>
         <form onSubmit={handleSubmit(onSubmit)}>
           <input className="textInput" name="original" ref={register({ required: true })} placeholder="Original"/>
           <input className="textInput" name="translation" ref={register({ required: true })} placeholder="Translation"/>
           <input id="submitButton" type="submit" value="Send"/>
         </form>
         </div>
-    </div>
+    </a.div>
   )
 };
